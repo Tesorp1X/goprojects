8b5a674811908ce517bb3c402989ba98a77a82a2
Tesorp1X <rakteam2014@gmail.com>
2025-04-17 00:24:55 +0700

    Storage interface and Note struct

--- 01-todo-list/internal/storage/storage.go
+++ 01-todo-list/internal/storage/storage.go
@@ -1,1 +1,18 @@
 package storage
+
+import "time"
+
+type Storage interface {
+	Save(string) error
+	GetNote(int) (*Note, error)
+	GetNotesList() ([]Note, error)
+	DeleteNote(int) error
+	AlterNote(int, Note)
+}
+
+type Note struct {
+	id        int
+	data      string
+	timeStamp time.Time
+	isClosed  bool
+}

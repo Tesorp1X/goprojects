78ae0eb183afcb6c50c03a0de9978aaf0fa20416
Tesorp1X <rakteam2014@gmail.com>
2025-04-21 00:08:35 +0700

    Consts, Settings and inits in main

--- 01-todo-list/cmd/tasks/main.go
+++ 01-todo-list/cmd/tasks/main.go
@@ -2,15 +2,56 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/Tesorp1X/goprojects/01-todo-list/internal/commands"
 )
 
+type Settings struct {
+	OutFile *os.File
+	ErrFile *os.File
+	Logger  *log.Logger
+}
+
+func InitSettings(outF, errF *os.File, log *log.Logger) *Settings {
+	return &Settings{OutFile: outF, ErrFile: errF, Logger: log}
+}
+
+var APP_SETTINGS *Settings
+
+const (
+	STD_STORAGE_FILE_DIR  = "/storage/"
+	STD_STORAGE_FILE_NAME = "tasks"
+	CSV_FILE_SUFFIX       = ".csv"
+	STD_LOG_FILE_NAME     = "/log/tasks.log"
+	LOG_MSG_PREFIX        = "INFO: "
+)
+
 func main() {
 	args := os.Args
-	command := args[1]
 
+	//TODO flags
+	//Init logger
+	logFile, errLog := os.Open(STD_LOG_FILE_NAME)
+	if errLog != nil {
+		panic("Log file is missing.")
+	}
+	defer logFile.Close()
+	logger := log.New(logFile, LOG_MSG_PREFIX, log.Ldate|log.Ltime)
+
+	//Init  Settings
+	APP_SETTINGS = InitSettings(os.Stdout, os.Stderr, logger)
+
+	//Init storage
+	storageFile, err := os.Open(STD_STORAGE_FILE_DIR + STD_STORAGE_FILE_NAME + CSV_FILE_SUFFIX)
+	if err != nil {
+		fmt.Fprintln(APP_SETTINGS.ErrFile, "Storage file is missing")
+	}
+	defer storageFile.Close()
+
+	//Parse command
+	command := args[1]
 	switch command {
 	case "add":
 		taskStr := "task"

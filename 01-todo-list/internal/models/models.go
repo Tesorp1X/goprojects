4cec9511cd82d28410780b74e63cdb547d752db9
Tesorp1X <rakteam2014@gmail.com>
2025-04-22 15:38:40 +0700

    New error messeges and time format

--- 01-todo-list/internal/models/models.go
+++ 01-todo-list/internal/models/models.go
@@ -14,3 +14,13 @@ type Settings struct {
 func InitSettings(outF, errF *os.File, log *log.Logger) *Settings {
 	return &Settings{OutFile: outF, ErrFile: errF, Logger: log}
 }
+
+const (
+	TimeFormat = "2006-01-02 15:04:05.999999999 -0700 MST"
+)
+
+// Errors
+const (
+	IdOutOfRangeError  = "error: given id is out range"
+	WrongNoteDataError = "error: couldn't create new note, because wrong data was given"
+)

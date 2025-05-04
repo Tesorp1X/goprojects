7d5dae8a5b5bd7c21c4c58751692a4b6196402ed
Tesorp1X <rakteam2014@gmail.com>
2025-05-04 14:46:29 +0700

    added basic logging

--- 01-todo-list/internal/commands/commands.go
+++ 01-todo-list/internal/commands/commands.go
@@ -2,34 +2,34 @@ package commands
 
 import (
 	"fmt"
-	"os"
 	"text/tabwriter"
 
+	"github.com/Tesorp1X/goprojects/01-todo-list/internal/models"
 	"github.com/Tesorp1X/goprojects/01-todo-list/internal/storage"
 )
 
 func AddCommand(s storage.Storage, task string) {
 	err := s.Save(task)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %s", err.Error())
-		// TODO logging
+		fmt.Fprintf(s.GetSettings().ErrFile, "error: %s", err.Error())
+		s.GetSettings().Logger.Fatalf("error: %s", err.Error())
 		return
 	}
-	fmt.Fprintf(os.Stdout, "Task: '%s' added to your list.", task)
+	fmt.Fprintf(s.GetSettings().OutFile, "Task: '%s' added to your list.", task)
 }
 
 func ListCommand(s storage.Storage, allFlag bool) {
 	notesList, err := s.GetNotesList()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %s", err.Error())
-		// TODO logging
+		fmt.Fprintf(s.GetSettings().ErrFile, "error: %s", err.Error())
+		s.GetSettings().Logger.Fatalf("error: %s", err.Error())
 		return
 	}
 	minwidth := 5         // minimal cell width including any padding
 	tabwidth := 8         // width of tab characters (equivalent number of spaces)
 	padding := 1          // padding added to a cell before computing its width
 	padchar := byte('\t') // ASCII char used for padding
-	w := tabwriter.NewWriter(os.Stdout, minwidth, tabwidth, padding, padchar, tabwriter.AlignRight)
+	w := tabwriter.NewWriter(s.GetSettings().OutFile, minwidth, tabwidth, padding, padchar, tabwriter.AlignRight)
 	fmt.Fprint(w, "|ID\tTask\tCreated\tDone\t|\n")
 	for _, note := range notesList {
 		if note.IsClosed() && !allFlag {
@@ -49,24 +49,24 @@ func ListCommand(s storage.Storage, allFlag bool) {
 func CompleteCommand(s storage.Storage, id int) {
 	note, err := s.GetNote(id)
 	if err != nil {
-		// TODO logging
-		fmt.Fprintf(os.Stderr, "Task with id %d not found.\n", id)
+		s.GetSettings().Logger.Fatalf("Task with id %d not found.\n", id)
+		fmt.Fprintf(s.GetSettings().ErrFile, "Task with id %d not found.\n", id)
 		return
 	}
 	note.Close()
 	err = s.AlterNote(*note)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Task with id %d not found.\n", id)
+		fmt.Fprintln(s.GetSettings().ErrFile, models.SomethingWentWrongError)
 		return
 	}
-	fmt.Fprintln(os.Stdout, "Task closed.")
+	fmt.Fprintln(s.GetSettings().OutFile, "Task closed.")
 }
 
 func DeleteCommand(s storage.Storage, id int) {
 	err := s.DeleteNote(id)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Task with id %d not found.", id)
+		fmt.Fprintf(s.GetSettings().ErrFile, "Task with id %d not found.", id)
 		return
 	}
-	fmt.Fprintln(os.Stdout, "Task deleted.")
+	fmt.Fprintln(s.GetSettings().OutFile, "Task deleted.")
 }
